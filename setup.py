4e23ae3fe9bc5d8cfc4bd1b6c7820c2d40c82211
holdenhao <holdenhao@gmail.com>
2017-10-28 16:19:51 +0800

    comment out requirement for paster

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ setup(name='wccpilgrimageblog.profiles',
       # The next two lines may be deleted after you no longer need
       # addcontent support from paster and before you distribute
       # your package.
-      setup_requires=["PasteScript"],
-      paster_plugins=["templer.localcommands"],
+      #setup_requires=["PasteScript"],
+      #paster_plugins=["templer.localcommands"],
 
       )
